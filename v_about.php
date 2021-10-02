fd4cb25417471f2733a55705ea21f3bb1babd4bc
Gholifpradityautama19 <91724356+Gholifpradityautama19@users.noreply.github.com>
2021-10-02 15:55:58 +0700

    Update v_about.php

--- v_about.php
+++ v_about.php
@@ -2,14 +2,14 @@
 	<h1><?php echo $judul ?></h1>
  	<h4>Nama</h4>
  	<ul type="disc">
- 		<li>Nama Depan : Putri</li>
- 		<li>Nama Belakang : Fajriah</li>
- 		<li>Nim : 19200278</li>
+ 		<li>Nama Depan : Gholif </li>
+ 		<li>Nama Belakang : Praditya</li>
+ 		<li>Nim : 19200990</li>
  	</ul>
  <br>
  	<h4>Alamat</h4>
  	<ul type="none">
- 		<li> Jln.Angsana Dalam No.78F</li>
+ 		<li> Jln.Mawar Melati Semuanya Indah No.90</li>
  	</ul>
  
  	<h4>Tempat Lahir</h4>
